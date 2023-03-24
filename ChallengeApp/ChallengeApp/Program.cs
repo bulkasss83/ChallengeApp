74afbe0bc411e7801a13b92ac1d749696c2e331e
Blazej Urbisz <blazej.urbisz@outlook.com>
2023-03-24 03:26:38 +0000

    Zadanie domowe dzien-4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//deklaracja imienia
+var name = "Ewa";
+
+//deklaracja wieku
+var age = 15;
+
+//deklaracja plci K=kobieta M= Mężczyzna
+char sex = 'M';
+
+
+if (sex == 'K' && age <= 30)
+{
+    Console.WriteLine("kobieta ponizej 30 lat");
+}
+else if (age == 33)
+{
+    if (name == "Ewa")
+    {
+        Console.WriteLine("Ewa 33 lat");
+    }
+}
+else if (sex == 'M' && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
