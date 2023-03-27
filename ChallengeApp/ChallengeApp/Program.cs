ec4dcab41eec59510622d7fffe5725c9fcca4915
Blazej Urbisz <blazej.urbisz@outlook.com>
2023-03-27 15:06:13 +0100

    Zadanie Dzien 5

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,25 +1,69 @@
-﻿//deklaracja imienia
-var name = "Ewa";
+﻿int number = 492433728;
+string numberAsString = number.ToString();
+char[] digits = numberAsString.ToArray();
 
-//deklaracja wieku
-var age = 15;
+int counter0 = 0;
+int counter1 = 0;
+int counter2 = 0;
+int counter3 = 0;
+int counter4 = 0;
+int counter5 = 0;
+int counter6 = 0;
+int counter7 = 0;
+int counter8 = 0;
+int counter9 = 0;
 
-//deklaracja plci K=kobieta M= Mężczyzna
-char sex = 'M';
-
-
-if (sex == 'K' && age <= 30)
+foreach (char digit in digits)
 {
-    Console.WriteLine("kobieta ponizej 30 lat");
-}
-else if (age == 33)
-{
-    if (name == "Ewa")
+    if (digit == '0')
     {
-        Console.WriteLine("Ewa 33 lat");
+        counter0++;
+    }
+    else if (digit == '1')
+    {
+        counter1++;
+    }
+    else if (digit == '2')
+    {
+        counter2++;
+    }
+    else if (digit == '3')
+    {
+        counter3++;
+    }
+    else if (digit == '4')
+    {
+        counter4++;
+    }
+    else if (digit == '5')
+    {
+        counter5++;
+    }
+    else if (digit == '6')
+    {
+        counter6++;
+    }
+    else if (digit == '7')
+    {
+        counter7++;
+    }
+    else if (digit == '8')
+    {
+        counter8++;
+    }
+    else if (digit == '9')
+    {
+        counter9++;
     }
 }
-else if (sex == 'M' && age < 18)
-{
-    Console.WriteLine("Niepełnoletni mężczyzna");
-}
+Console.WriteLine(number);
+Console.WriteLine("0=" + counter0);
+Console.WriteLine("1=" + counter1);
+Console.WriteLine("2=" + counter2);
+Console.WriteLine("3=" + counter3);
+Console.WriteLine("4=" + counter4);
+Console.WriteLine("5=" + counter5);
+Console.WriteLine("6=" + counter6);
+Console.WriteLine("7=" + counter7);
+Console.WriteLine("8=" + counter8);
+Console.WriteLine("9=" + counter9);
