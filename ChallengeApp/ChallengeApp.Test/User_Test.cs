55148b7d6f45676cd6f4f2e203849ad6747a3015
Blazej Urbisz <blazej.urbisz@outlook.com>
2023-04-24 12:05:03 +0100

    Zadanie Dzien 7

--- ChallengeApp/ChallengeApp.Test/User_Test.cs
+++ ChallengeApp/ChallengeApp.Test/User_Test.cs
@@ -3,7 +3,7 @@ namespace ChallengeApp.Test
     public class Tests
     {
         [Test]
-        public void WhenUserCollectTwoScores_ShouldCorrectResult()
+        public void WhenUserCollectTwoScores_ShouldReturnCorrectResult()
         {
             //arrange
             var user = new Employee("Wincenty", "Kowalski", "33");
@@ -15,8 +15,59 @@ namespace ChallengeApp.Test
             var result = user.Result;
 
             //assert
-            Assert.AreEqual(20, result);
+            Assert.That(result, Is.EqualTo(20));
+        }
+        [Test]
+        public void WhenUserCollectOnePenaltyScore_ShouldReturnCorrectResult()
+        {
+            //arrange
+            var user = new Employee("Wincenty", "Kowalski", "33");
 
+            user.AddScore(10);
+            user.AddScore(10);
+            user.AddScore(-1);
+
+            //act
+            var result = user.Result;
+
+            //assert
+            Assert.That(result, Is.EqualTo(19));
+        }
+        [Test]
+        public void WhenUserCollectSomePenaltyScore_ShouldReturnCorrectResult()
+        {
+            //arrange
+            var user = new Employee("Wincenty", "Kowalski", "33");
+
+            user.AddScore(10); // Time keeping
+            user.AddScore(-6);  // Sickness
+            user.AddScore(5);  // Efficiency
+            user.AddScore(-9);  // Productivity
+            user.AddScore(10);  // Cooperation
+
+            //act
+            var result = user.Result;
+
+            //assert
+            Assert.That(result, Is.EqualTo(10));
+        }
+        [Test]
+        public void WhenUserCollectOnlyPenaltyScore_ShouldReturnCorrectResult()
+        {
+            //arrange
+            var user = new Employee("Wincenty", "Kowalski", "33");
+
+            user.AddScore(-5); // Time keeping
+            user.AddScore(-6);  // Sickness
+            user.AddScore(0);  // Efficiency
+            user.AddScore(-2);  // Productivity
+            user.AddScore(0);  // Cooperation
+
+            //act
+            var result = user.Result;
+
+            //assert
+            Assert.That(result, Is.EqualTo(-13));
         }
     }
 }
