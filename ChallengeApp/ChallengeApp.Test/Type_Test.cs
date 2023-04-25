d515f20280efc897487af36b3f16e4e7aa9a00fd
Blazej Urbisz <blazej.urbisz@outlook.com>
2023-04-25 02:36:36 +0100

    Zadanie Dzien 8

---
+++ ChallengeApp/ChallengeApp.Test/Type_Test.cs
@@ -0,0 +1,59 @@
+﻿
+namespace ChallengeApp.Test
+{
+    public class Type_Test
+    {
+
+        [Test]
+        public void TestingTwoTheSameStrings()
+        {
+            string number1 = "Adam";
+            string number2 = "Adam";
+
+            Assert.AreEqual(number1, number2);
+        }
+        [Test]
+        public void TestingTwoTheSameIntigers()
+        {
+            int number1 = 5;
+            int number2 = 5;
+
+            Assert.AreEqual(number1, number2);
+        }
+        [Test]
+        public void TestingTwoNotTheSameUshort()
+        {
+            ushort number1 = 4;
+            ushort number2 = 5;
+
+            Assert.AreNotEqual(number1, number2);
+        }
+        [Test]
+        public void TestingTwoTheSameFloat()
+        {
+            float number1 = 4.132435F;
+            float number2 = 4.132435F;
+
+            Assert.AreEqual(number1, number2);
+        }
+        [Test]
+        public void TestingTwoNotTheSameDouble()
+        {
+            double number1 = 1.243546654;
+            double number2 = 1.243546576;
+
+            Assert.AreNotEqual(number1, number2);
+        }
+
+        private Employee GetUser(string name)
+        
+        {
+            return new Employee(name); 
+        }
+            
+    }
+
+}
+
+
+
